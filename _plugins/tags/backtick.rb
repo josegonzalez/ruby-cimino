c81d0bf666a978422bd10905f191a7b9b6c80e06
Jose Diaz-Gonzalez <myaccounts@savant.be>
2012-04-22 17:01:32 -0400

    Added backtick.rb, enabling usage of ``` blocks for syntax highlighting"

---
+++ _plugins/tags/backtick.rb
@@ -0,0 +1,27 @@
+# Title: BacktickTag
+# Original: https://github.com/imathis/octopress/blob/master/plugins/backtick_code_block.rb
+# Description: Allow placing codeblocks within three ```, github-style
+
+require './_plugins/extensions/post_filter'
+require 'rubypants'
+
+module Jekyll
+  class BacktickCodeBlockFilter < PostFilter
+    def initialize(config)
+      @tag = "highlight"
+      if config.key?('backtick') and config['backtick'].key?('tag')
+        @tag = config['backtick']['tag']
+      end
+      super
+    end
+
+    def pre_render(post)
+      post.content = post.content.gsub(/^`{3} (.*)$/, "{% #{@tag} \\1 %}")
+      post.content = post.content.gsub(/^`{3}$/, "{% end#{@tag} %}")
+    end
+
+    def post_render(post)
+      post.content
+    end
+  end
+end
