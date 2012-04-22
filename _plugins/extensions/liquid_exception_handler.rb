61ab65d76fc6b34e8276bfd8331942ff661d8407
Jose Diaz-Gonzalez <myaccounts@savant.be>
2012-04-22 17:02:11 -0400

    Suppress liquid exceptions using the LiquidExceptionHandler plugin

---
+++ _plugins/extensions/liquid_exception_handler.rb
@@ -0,0 +1,8 @@
+# Title: Liquid Exception Handler Jekyll Extension
+# Description: Fixes https://github.com/mojombo/jekyll/issues/46 and https://github.com/mojombo/jekyll/issues/388
+
+module Jekyll
+  class Post
+    attr_accessor :name
+  end
+end
