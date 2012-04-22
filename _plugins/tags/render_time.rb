3af3d13d2e0e88ad2821efeb9f40d306436bc042
Jose Diaz-Gonzalez <myaccounts@savant.be>
2012-04-22 17:00:01 -0400

    Added sample render_time.rb tag plugin

---
+++ _plugins/tags/render_time.rb
@@ -0,0 +1,19 @@
+# Title: RenderTimeTag
+# Source: https://github.com/mojombo/jekyll/wiki/Plugins
+# Description: Sample `render_time` tag
+
+module Jekyll
+  class RenderTimeTag < Liquid::Tag
+
+    def initialize(tag_name, text, tokens)
+      super
+      @text = text
+    end
+
+    def render(context)
+      "#{@text} #{Time.now}"
+    end
+  end
+end
+
+Liquid::Template.register_tag('render_time', Jekyll::RenderTimeTag)
